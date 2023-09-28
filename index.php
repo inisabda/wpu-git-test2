54fe9c4a61e36d587523784922e7547189bfad77
inisabda <inisabda@gmail.com>
2023-09-28 15:35:22 +0700

    tambah li

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>hello</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias repellat quod sint dicta, cumque, doloremque, dolores suscipit omnis ea laborum ex delectus similique accusantium? Sapiente in est nobis architecto fugiat?</p>
+    <li>ddcdv</li>
 </body>
 </html>
